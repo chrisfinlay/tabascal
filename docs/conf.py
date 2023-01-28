d442658653f11f1b1bb1ac93a9f7782772e87e9c
Chris Finlay <chrisfinlay93@gmail.com>
2023-01-28 12:33:56 +0100

    Updated path insert.

--- docs/conf.py
+++ docs/conf.py
@@ -4,7 +4,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../tabascal/'))
+sys.path.insert(0, os.path.abspath('../tabascal/'))
 
 from datetime import date
 
