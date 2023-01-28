39c3bd19c89f6eca4828f79f6cdeb55c51ca4fa4
Chris Finlay <chrisfinlay93@gmail.com>
2023-01-28 12:28:50 +0100

    Add docs requirements files and add the package to the documentation build path.

--- docs/conf.py
+++ docs/conf.py
@@ -2,6 +2,10 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../tabascal/'))
+
 from datetime import date
 
 # -- Project information -----------------------------------------------------
