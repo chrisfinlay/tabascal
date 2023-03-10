d1633b94d0df28301600a9354c70e0f281d69f30
Chris Finlay <chrisfinlay93@gmail.com>
2023-03-10 12:40:24 +0100

    Moved example sim.py to examples directory.

--- tabascal/examples/sim.py
+++ tabascal/examples/sim.py
@@ -4,7 +4,6 @@ import os
 import jax.numpy as jnp
 from jax import random
 
-from tabascal import Observation
 from tabascal.utils.tools import (
     generate_random_sky,
     load_antennas,
@@ -17,9 +16,14 @@ parser.add_argument(
     "--f_name", default="test", help="File name to save the observations."
 )
 parser.add_argument("--o_path", default="./", help="Path to save the observations.")
-parser.add_argument("--noise", default=0.65, type=float, help="Noise level in Jy.")
-parser.add_argument("--T_0", default=440.0, type=float, help="Start time in seconds.")
-parser.add_argument("--dT", default=2.0, type=float, help="Time step in seconds.")
+parser.add_argument(
+    "--SEFD",
+    default=420.0,
+    type=float,
+    help="System Equivalent flux density in Jy. Same across frequency and antennas.",
+)
+parser.add_argument("--t_0", default=440.0, type=float, help="Start time in seconds.")
+parser.add_argument("--delta_t", default=2.0, type=float, help="Time step in seconds.")
 parser.add_argument("--N_t", default=10, type=int, help="Number of time steps.")
 parser.add_argument(
     "--N_int", default=16, type=int, help="Number of integration samples."
@@ -33,13 +37,16 @@ parser.add_argument(
 parser.add_argument(
     "--grdRFI", default=True, type=str2bool, help="Include ground-based RFI source."
 )
+parser.add_argument(
+    "--backend", default="dask", type=str, help="Use pure JAX or Dask backend."
+)
 
 args = parser.parse_args()
 f_name = args.f_name
 output_path = args.o_path
-noise = args.noise
-T_0 = args.T_0
-dT = args.dT
+SEFD = args.SEFD
+t_0 = args.t_0
+dT = args.delta_t
 N_t = args.N_t
 N_int = args.N_int
 N_ant = args.N_a
@@ -48,26 +55,33 @@ Rkey = args.Rkey
 satRFI = args.satRFI
 grdRFI = args.grdRFI
 
+if args.backend.lower == "jax":
+    from tabascal.jax.observation import Observation
+else:
+    from tabascal.dask.observation import Observation
+
 ants_enu = random.permutation(random.PRNGKey(19), load_antennas("MeerKAT"))[:N_ant]
+
+times = jnp.arange(t_0, t_0 + N_t * dT, dT)
+freqs = jnp.array([1.227e9])
+
 obs = Observation(
     latitude=-30.0,
     longitude=21.0,
     elevation=1050.0,
     ra=27.0,
     dec=15.0,
-    times=jnp.arange(T_0, T_0 + N_t * dT, dT),
-    freqs=jnp.array([1.227e9]),
+    times=times,
+    freqs=freqs,
+    SEFD=SEFD * freqs,
     ENU_array=ants_enu,
     n_int_samples=N_int,
+    time_chunk=len(times) // 5,
+    freq_chunk=1,
 )
 
-n_src = 100
-mean_I = 0.1
-
-I, d_ra, d_dec = generate_random_sky(n_src, mean_I, obs.fov, obs.syn_bw)
-spectral_indices = 0.7 + 0.2 * random.normal(random.PRNGKey(101), (n_src,))
-I = I[:, None] * (obs.freqs / obs.freqs[0])[None, :] ** (
-    -1.0 * spectral_indices[:, None]
+I, d_ra, d_dec = generate_random_sky(
+    n_src=100, mean_I=0.1, freqs=obs.freqs, fov=obs.fov, beam_width=obs.syn_bw
 )
 
 obs.addAstro(I=I, ra=obs.ra + d_ra, dec=obs.dec + d_dec)
@@ -98,7 +112,6 @@ if grdRFI:
 obs.addGains(G0_mean=1.0, G0_std=0.05, Gt_std_amp=1e-5, Gt_std_phase=jnp.deg2rad(1e-3))
 
 obs.calculate_vis()
-obs.addNoise(noise=noise, key=random.PRNGKey(104 + int(T_0)))
 
 f_name = (
     f"{f_name}_obs_{obs.n_ant:0>2}A_{obs.n_time:0>3}T_{obs.n_freq:0>3}F"
