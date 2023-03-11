276fa804931136066e761627768a446425de01cb
Chris Finlay <chrisfinlay93@gmail.com>
2023-03-11 13:39:31 +0100

    Formatting and requirements update.

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 from setuptools import setup, find_packages
 
-description = '''Trajectory based Radio Frequency Interference (RFI) subtraction
+description = """Trajectory based Radio Frequency Interference (RFI) subtraction
                  and calibration using Bayesian methods for radio
-                 interferometeric data.'''
+                 interferometeric data."""
 
-setup(name='tabascal',
-      version='0.0.1',
-      description=description,
-      url='http://github.com/chrisfinlay/tabascal',
-      author='Chris Finlay',
-      author_email='christopher.finlay@unige.ch',
-      license='MIT',
-      packages=find_packages(),
-      package_data={'tabascal': ['tabascal/data/*']},
-      # scripts=['bin/'],
-      install_requires=['jax',
-                        'h5py'],
-      zip_safe=False)
+setup(
+    name="tabascal",
+    version="0.0.1",
+    description=description,
+    url="http://github.com/chrisfinlay/tabascal",
+    author="Chris Finlay",
+    author_email="christopher.finlay@unige.ch",
+    license="MIT",
+    packages=find_packages(),
+    package_data={"tabascal": ["tabascal/data/*"]},
+    # scripts=['bin/'],
+    install_requires=["jax", "jaxlib", "dask[array]", "xarray", "dask-ms"],
+    zip_safe=False,
+)
