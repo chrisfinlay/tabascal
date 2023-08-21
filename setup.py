3a979259e3a21bbcc6e63cfe3c324c7663eee377
Chris Finlay <chrisfinlay93@gmail.com>
2023-08-21 14:44:17 +0900

    Add new config files and licence.

--- setup.py
+++ setup.py
@@ -1,23 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-description = """Trajectory based Radio Frequency Interference (RFI) subtraction
-                 and calibration using Bayesian methods for radio
-                 interferometeric data."""
-
-setup(
-    name="tabascal",
-    version="0.0.1",
-    description=description,
-    url="http://github.com/chrisfinlay/tabascal",
-    author="Chris Finlay",
-    author_email="christopher.finlay@unige.ch",
-    license="MIT",
-    packages=find_packages(),
-    package_data={"tabascal": ["tabascal/data/*"]},
-    entry_points="""
-        [console_scripts]
-        sim-vis=tabascal.examples.target_observation:cli
-    """,
-    install_requires=["jax", "dask", "xarray", "dask-ms"],
-    zip_safe=False,
-)
+if __name__ == "__main__":
+    setup()
